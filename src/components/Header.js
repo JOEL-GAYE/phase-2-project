1c7ef2e777d0fee16b06bed5f80c75ff8831d6ee
JOEL <joelgaye17@gmail.com>
2024-11-11 19:43:38 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header () {
+  
+    return (
+      <div>
+        <h3 color='grey'>Mkulima solutions</h3>
+      </div>
+    )
+ 
+}
+
+export default Header
