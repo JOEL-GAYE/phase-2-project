7d9970084c5d62e30ef32946230447dfa8844637
JOEL <joelgaye17@gmail.com>
2024-11-12 01:17:09 +0300

    night nurse

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
+
+function Footer() {
   return (
     <>
-     <div id="sub">
-            <h3 id="subscribe">Subscribe</h3>
-            <p>Leave your email here for regular tips, notifications, and new products!</p>
-            <form id="subscribeForm">
-                <div class="email-form">
-                    <span class="form-control-wrap">
-                        <input type="email" name="email" id="email" size="20" class="form-groupform-control form-control-lg"
-                            placeholder="Your working email" required/>
-                    </span> <br/>
-                    <button type="submit" class="sub-submit">Subscribe
-                        <i class="fas fa-chevron-right text-danger"></i>
-                    </button>
-                </div>
-            </form>
-        </div>
-    <div id='footer'>
-     &copy; 2024 Mkulima Solutions. All rights reserved.
-    </div></>
-  )
+      <div id="sub">
+        <h3 id="subscribe">Subscribe</h3>
+        <p>
+          Leave your email here for regular tips, notifications, and new
+          products!
+        </p>
+        <form id="subscribeForm">
+          <div class="email-form">
+            <span class="form-control-wrap">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                size="20"
+                class="form-groupform-control form-control-lg"
+                placeholder="Enter a valid Email"
+                required
+              />
+            </span>{" "}
+            <br />
+            <button type="submit" class="sub-submit" >
+              Subscribe
+              <i class="fas fa-chevron-right text-danger"></i>
+            </button>
+          </div>
+        </form>
+      </div>
+      <div id="footer">&copy; 2024 Mkulima Solutions. All rights reserved.</div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
