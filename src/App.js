fdd824a0f28cc6545c19512a15a0af370a6da716
JOEL <joelgaye17@gmail.com>
2024-11-11 21:22:46 +0300

    images

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Search from './components/Search';
+// import Disease from './components/Disease';
 
 function App() {
   return (
     <div>
-      <Header/>
-     < Search/>
-      <Footer/>
+      <Header />
+      <Search />
+      {/* <Disease /> */}
+      <Footer />
     </div>
-  )
-    
-  
+  );
 }
 
 export default App;
